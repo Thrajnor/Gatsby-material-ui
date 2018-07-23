e4d364d0d14077c7c22a2abe334200f46a262b9d
MarcinMac <jimrajnor@gmail.com>
2018-07-23 17:58:25 +0200

    make working prototype of material-ui

--- default-gatsby-with-material-ui/src/pages/index.js
+++ default-gatsby-with-material-ui/src/pages/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import withRoot from '../withRoot';
+
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
 
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="#">Go to page 2</Link>
+    <Button variant="contained" color="secondary" >Any button</Button>
   </div>
 )
 
-export default IndexPage
+export default withRoot(withStyles(styles)(IndexPage))
