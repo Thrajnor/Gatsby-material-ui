b9cb9f24339f3ad7ee88a654bd7e1b5b1ab0ce83
MarcinMac <jimrajnor@gmail.com>
2018-07-23 15:22:50 +0200

    couldn't make it work

--- test-plugin-material-ui/gatsby-config.js
+++ test-plugin-material-ui/gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 
+  {
+    resolve: 'gatsby-plugin-material-ui',
+    options: {
+      pathToTheme: 'src/themes/default',
+    },
+  },],
 }
--- test-plugin-material-ui/src/pages/index.js
+++ test-plugin-material-ui/src/pages/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import createMuiTheme from 'material-ui/styles/createMuiTheme'
+
+import purple from 'material-ui/colors/purple'
+import green from 'material-ui/colors/green'
+import red from 'material-ui/colors/red'
+
+export default createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+    error: red,
+  },
+})
 
 const IndexPage = () => (
   <div>
