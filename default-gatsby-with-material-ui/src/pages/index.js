1844934710c3eee99bb84f414ed257e04c21acdd
MarcinMac <jimrajnor@gmail.com>
2018-07-30 15:16:32 +0200

    Integrate material-kit-react in to gatsby default

--- default-gatsby-with-material-ui/src/pages/index.js
+++ default-gatsby-with-material-ui/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
+import Button from '../components/CustomButtons/Button.jsx';
 import { withStyles } from '@material-ui/core/styles';
 import withRoot from '../withRoot';
 
@@ -17,7 +18,8 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="#">Go to page 2</Link>
-    <Button variant="contained" color="secondary" >Any button</Button>
+    {/* <Button variant="contained" color="secondary" >Any button</Button> */}
+    <Button type="button" color="rose" >Your Material-kit-react button!</Button>
   </div>
 )
 
