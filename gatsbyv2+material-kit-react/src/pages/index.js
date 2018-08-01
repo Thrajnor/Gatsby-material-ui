57ec22fa9770392e0c1af2b2f12d5047cbffb87e
MarcinMac <jimrajnor@gmail.com>
2018-08-01 15:01:41 +0200

    improve code

--- gatsbyv2+material-kit-react/src/pages/index.js
+++ gatsbyv2+material-kit-react/src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
 // depo for vanila button V
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 
 // depo for material-kit-react
-import Button from '../components/CustomButtons/Button.jsx';
-
-import { withStyles } from '@material-ui/core/styles';
-import withRoot from '../withRoot';
+// import Button from '../components/CustomButtons/Button.jsx';
 
 import Layout from '../components/layout'
 
@@ -23,10 +20,9 @@ const IndexPage = () => (
     <h1>Hi Czapel!</h1>
     <p>Welcome to your new Gatsby site which is using features form your favorite material-kit-react.</p>
     <p>Now go build something great.</p>
-    {/* This is vanilla @material-ui/core button
-     <Button variant="contained" color="secondary" >Any button</Button> */}
-    <Button type="button" color="rose" >Your Material-kit-react button!</Button>
+    <Button variant="contained" color="secondary" >Any button</Button>
+    {/* <Button type="button" color="rose" >Your Material-kit-react button!</Button> */}
   </Layout>
 )
 
-export default withRoot(withStyles(styles)(IndexPage))
+export default IndexPage
