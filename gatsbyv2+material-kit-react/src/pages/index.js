3547149c5f2b8055dae0936bbe67c9517d1f680b
MarcinMac <jimrajnor@gmail.com>
2018-08-02 15:08:24 +0200

    cosmetic changes

--- gatsbyv2+material-kit-react/src/pages/index.js
+++ gatsbyv2+material-kit-react/src/pages/index.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
 // depo for vanila button V
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 
 // depo for material-kit-react
-// import Button from '../components/CustomButtons/Button.jsx';
+import Button from '../components/CustomButtons/Button.jsx';
 
 import Layout from '../components/layout'
 
-const styles = theme => ({
-  root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
-  },
-});
+// const styles = theme => ({
+//   root: {
+//     textAlign: 'center',
+//     paddingTop: theme.spacing.unit * 20,
+//   },
+// });
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi Czapel!</h1>
-    <p>Welcome to your new Gatsby site which is using features form your favorite material-kit-react.</p>
+    <p>Welcome to your new Gatsby v2 site which is using features form your favorite material-kit-react.</p>
     <p>Now go build something great.</p>
-    <Button variant="contained" color="secondary" >Any button</Button>
-    {/* <Button type="button" color="rose" >Your Material-kit-react button!</Button> */}
+    {/* <Button variant="contained" color="secondary" >Any button</Button> */}
+    <Button type="button" color="rose" >Your Material-kit-react button!</Button>
   </Layout>
 )
 
