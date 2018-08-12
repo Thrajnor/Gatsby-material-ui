b2b16022db574f4bd5f76fe7cead5ea8cef68578
MarcinMac <jimrajnor@gmail.com>
2018-08-12 19:26:13 +0200

    repair some issues

--- landingpage/src/pages/index.jsx
+++ landingpage/src/pages/index.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-
-import "assets/scss/material-kit-react.css?v=1.1.0";
 import Layout from "components/layout/layout";
 import LandingPage from "pages/LandingPage/LandingPage";
 
 class Index extends React.Component {
   render() {
     return (
-      <Layout>
         <LandingPage></LandingPage>
-      </Layout>
     );
   }
 }
